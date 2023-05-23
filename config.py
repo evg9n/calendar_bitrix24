718ec7c0b03493aa4bf925cb33c6d02e2c49187c
Evgen <e.evgeny999@gmail.com>
2023-05-23 12:22:00 +0300

    update

--- config.py
+++ config.py
@@ -1,17 +1,2 @@
-# TOKEN - это Ключ API Wazzup
-TOKEN = 'TOKEN'
-# CHANNEL_ID можно получить из url канала
-CHANNEL_ID = "CHANNEL_ID"
-# Номер на который нужно отправлять уведомления в whatsapp
-PHONE = "PHONE"
-
-# Входящий вебхук с правами на календарь
-WEBHOOK = "WEBHOOK"
-
-# id пользователя
-OWNER_ID = 7
-# интервал между запросами
-SLEEP_SECONDS = 30
-# на сколько дней вперед проверять
-FUTURE_DAYS = 14
+dscfds
 
